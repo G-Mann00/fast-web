03707665475fabc9759119ced1318a974b78bfac
G-Mann00 <daniloluswi@gmail.com>
2024-03-20 21:29:08 -0600

    Add: agregando archivos iniciales

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      }
+    },
+
+    colors: {
+      "FAST-Orange": "#FF7622",
+      "FAST-Grey": "#98A8B8",
+      "FAST-DarkGrey": "#6B6E82",
+      "FAST-DarkBlue": "#121223",
+      "FAST-WhiteCream": "#F0F5FA"
+    },
+  },
+  plugins: [],
+}
+
